8cac233e441a6dab2cf8f3e60676dc202b0fc89d
Nazacheres <nazacheres@gmail.com>
2022-03-11 14:01:57 +0000

    [note] Change CI system to work with tags.

--- scripts/updateVersion.sh
+++ scripts/updateVersion.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-branch="$(git rev-parse --abbrev-ref HEAD)"
+tag="$(git tag -l | egrep v | tail -1)"
 regexVersion='[0-9]\+'
-version=($(echo "Tools version: $branch" | grep -o $regexVersion))
+version=($(echo "$tag" | grep -o $regexVersion))
 
 if [ ${#version[@]} -eq 0 ]; then
   version=(0 0 1 1)
   newBuildVersion=$(git rev-list --count HEAD)
 else
-  newBuildVersion=$(git rev-list --count HEAD ^$branch)
+  newBuildVersion=$(git rev-list --count $(git rev-list -n 1 $tag)..HEAD)
 fi
 
 
