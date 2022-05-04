29ecbbd9c45496346adbc3b88cc6b3e178fcc95a
Nazacheres <nazacheres@gmail.com>
2022-05-04 15:31:57 +0200

    [note] Added UnidirectionalRouterDelegate.

---
+++ lib/router/unidirectional_router_delegate.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tools/extensions/nullability_extensions.dart';
+
+abstract class UnidirectionalRouterDelegate<ARS, PSA> extends RouterDelegate<ARS> with ChangeNotifier, PopNavigatorRouterDelegateMixin<ARS> {
+  UnidirectionalRouterDelegate({required UnidirectionalRouterState<ARS, PSA?> initialState}) :
+        _stateToProcess = initialState,
+        _initialState = initialState;
+
+  final List<UnidirectionalRouterPage> _stack = [];
+
+
+  final UnidirectionalRouterState _initialState;
+  UnidirectionalRouterState? _stateToProcess;
+
+  @override
+  final navigatorKey = GlobalKey<NavigatorState>();
+
+  void reportRouterStateChanges(ARS appRouteState, [PSA? stateArguments]) {
+    _stateToProcess = UnidirectionalRouterState(appRouteState, stateArguments);
+    notifyListeners();
+  }
+
+  @override
+  ARS get currentConfiguration {
+    return tryOrNull(() => _stack.last).state ?? _initialState.state;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_stateToProcess != null && _stateToProcess?.state != tryOrNull(() => _stack.last).state) {
+      final willBeProcessingState = _stateToProcess;
+      _stateToProcess = null;
+      updateStack(context, willBeProcessingState!);
+    }
+
+    late final pages = _stack.map((e) => e.page).toList();
+    late final states = _stack.map((e) => e.page).toList();
+
+    return Navigator(
+      key: navigatorKey,
+      pages: pages,
+      onPopPage: (route, result) {
+        if (!route.didPop(result)) {
+          return false;
+        }
+
+        if (states.length > 1) {
+          _stack.removeLast();
+        }
+
+        notifyListeners();
+        return true;
+      },
+    );
+  }
+
+  /// Once the Future completes, currentConfiguration will be pulled again, and it should be up to date.
+  /// So navigational blocs should have required values already in the state.
+  @override
+  Future<void> setNewRoutePath(ARS configuration) async {
+    reportRouterStateChanges(configuration);
+  }
+
+  void updateStack(BuildContext context, UnidirectionalRouterState stateToProcess);
+
+}
+
+class UnidirectionalRouterState<ARS, PSA> {
+  UnidirectionalRouterState(this.state, this.arguments);
+
+  final ARS state;
+  final PSA? arguments;
+}
+
+class UnidirectionalRouterPage<ARS> {
+  UnidirectionalRouterPage(this.state, this.page);
+
+  final ARS state;
+  final Page page;
+}
